0af883768e43cbf81cd85cd021851bb900dde201
jhincapie197 <145515467+jhincapie197@users.noreply.github.com>
2024-08-14 20:57:46 -0500

    Repaso_Python
    Clase 14 agosto 2024

---
+++ RepasoPython/VARIABLE.py
@@ -0,0 +1,29 @@
+#Python identifica automaticamente el tipo de variable
+"""precio = 10.2
+print(type(precio))"""
+
+#Division con / arroja resultado con decimales, con // arroja resultado entero
+"""division = 20//5
+print(division)"""
+
+#resto% de una división
+"""resto = 7%2
+print(resto)"""
+
+#Asignacion
+"""num1 = 5
+num2 = 3
+num1 += 15 #num1 = num1 + 15
+print(num1)
+
+num2 -= 2
+print(num2)"""
+
+#Potenciacion
+"""potencia = 3**3
+print(potencia)"""
+
+#Imprimir si el numero asignado a una variable es par
+"""num = float(input("Ingrese un numero entero positivo: "))
+resto = num % 2
+print(resto)"""
---
+++ RepasoPython/condicional.py
@@ -0,0 +1,36 @@
+#Condicionales (IF - ELIF):
+"""numero = int(input("Ingrese un numero entero positivo: "))
+if numero % 2 == 0:
+    print("El número es par")
+    print(f"El número {numero} es par")
+    print("El número " + str(numero) + " es par")
+    print("El número", numero, "es par")
+else:
+    print("El número es impar")
+    print(f"El número {numero} es impar")
+    print("El número", numero, "es impar")"""
+
+
+#Solicitar nombre, edad de un usuario y si el usuario tiene o no pasaporte.
+#Si el usuario es mayor de edad y tiene pasaporte debe mostrar "Puede realizar viajes internacionales", de lo contrario "No puede abandonar el pais"
+"""nombre = input("Ingrese su nombre completo: ")
+edad = int(input("Ingrese su edad: "))
+pasaporte = input("¿Tiene pasaporte? Si/No: ")
+
+if edad >= 18 and pasaporte == "Si":
+    print(f"El usuario {nombre} puede realizar viajes internacionales")
+else:
+    print(f"El usuario {nombre} no puede abandonar el pais")"""
+
+#Elif: se utiliza para validar 2 o mas condiciones
+
+nombre = input("Ingrese su nombre completo: ")
+edad = int(input("Ingrese su edad: "))
+pasaporte = input("¿Tiene pasaporte? Si/No: ")
+
+if edad >= 18 and pasaporte == "Si":
+    print(f"El usuario {nombre} puede realizar viajes internacionales")
+elif edad <18 and pasaporte == "Si":
+    print(f"El usuario {nombre} puede realizar viajes internacionales si tiene permisos de sus acudientes")
+else:
+    print(f"El usuario {nombre} no puede abandonar el pais")
